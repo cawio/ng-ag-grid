c946ba230b3072ab41fd49f43dd0e2a185a17034
Simon Berner <sberner9@gmail.com>
2024-02-13 14:11:57 +0100

    feat: :sparkles: Add routes for example grid component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ExampleGridComponent } from './features/example-grid/example-grid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'example',
+    pathMatch: 'full',
+  },
+  {
+    path: 'example',
+    component: ExampleGridComponent,
+  },
+];
