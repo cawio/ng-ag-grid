43348824cc181d3ef5cb9d5e5511426bfde5cbc1
Simon Berner <sberner9@gmail.com>
2024-02-13 14:10:37 +0100

    feat: :sparkles: Add example-grid component

---
+++ src/app/features/example-grid/example-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './example-grid.component.html',
+  styleUrl: './example-grid.component.scss'
+})
+export class ExampleGridComponent {
+
+}
